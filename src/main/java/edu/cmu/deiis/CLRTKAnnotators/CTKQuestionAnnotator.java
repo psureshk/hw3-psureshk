17e247d1a4b17e4a371d6c6c6119c2c01cbe1733
psureshk <psureshk@andrew.cmu.edu>
2013-10-07 17:19:41 -0700

    hw3-CLearTK

---
+++ src/main/java/edu/cmu/deiis/CLRTKAnnotators/CTKQuestionAnnotator.java
@@ -0,0 +1,19 @@
+package edu.cmu.deiis.CLRTKAnnotators;
+
+import java.util.Iterator;
+
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.FSIndex;
+import org.apache.uima.jcas.JCas;
+import org.cleartk.ne.type.NamedEntityMention;
+
+public class CTKQuestionAnnotator extends JCasAnnotator_ImplBase{
+
+  @Override
+  public void process(JCas aJCas) throws AnalysisEngineProcessException {
+    // TODO Auto-generated method stub
+   
+  }
+
+}
